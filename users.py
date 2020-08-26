875067b0c019a034fe46a7d926e43b825a3ed5d1
mtumasov <66528185+mtumasov@users.noreply.github.com>
2020-08-26 23:17:32 +0500

    Add files via upload

---
+++ users.py
@@ -0,0 +1,92 @@
+# импортируем модули стандартной библиотеки uuid и datetime
+import uuid
+
+# импортируем библиотеку sqlalchemy и некоторые функции из нее
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# константа, указывающая способ соединения с базой данных
+DB_PATH = 'sqlite:///sochi_athletes.sqlite3'
+# базовый класс моделей таблиц
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    # задаем название таблицы
+    __tablename__ = 'user'
+    # идентификатор пользователя, первичный ключ
+    id = sa.Column(sa.INTEGER, primary_key=True)
+    # имя пользователя
+    first_name = sa.Column(sa.Text)
+    # фамилия пользователя
+    last_name = sa.Column(sa.Text)
+    # пол пользователя
+    gender = sa.Column(sa.Text)
+    # адрес электронной почты пользователя
+    email = sa.Column(sa.Text)
+    # дата рождения пользователя
+    birthdate = sa.Column(sa.Text)
+    # рост пользователя
+    height = sa.Column(sa.REAL)
+
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
+    """
+    # создаем соединение к базе данных
+    engine = sa.create_engine(DB_PATH)
+    # создаем фабрику сессию
+    session = sessionmaker(engine)
+    # возвращаем сессию
+    return session()
+
+
+def request_data():
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    """
+    # выводим приветствие
+    print("Привет! Я запишу твои данные!")
+    # запрашиваем у пользователя данные
+    first_name = input("Введи своё имя: ")
+    last_name = input("А теперь фамилию: ")
+    gender = input("Введите ваш пол Male или Female: ")
+    email = input("Мне еще понадобится адрес твоей электронной почты: ")
+    birthdate = input("Введите дату вашего рождения  в формате YYYYMMDD: ")
+    height = input("Введите ваш рост в см: ")
+    # генерируем идентификатор пользователя и сохраняем его строковое представление
+    user_id = str(uuid.uuid4())
+    # создаем нового пользователя
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        email=email,
+        birthdate=birthdate,
+        height=height,
+    )
+    # возвращаем созданного пользователя
+    return user
+
+
+def main():
+    """
+    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    """
+    session = connect_db()
+    # запрашиваем данные пользоватлея
+    user = request_data()
+    # добавляем нового пользователя в сессию
+    session.add(user)
+    # сохраняем все изменения, накопленные в сессии
+    session.commit()
+    print("Спасибо, данные сохранены!")
+
+
+if __name__ == "__main__":
+    main()
